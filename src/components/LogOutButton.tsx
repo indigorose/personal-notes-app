52fe574c2033964a109685c8c803e512c0be1776
indigorose <indigorose049@gmail.com>
2025-03-24 21:35:41 +0000

    Implement LogOutButton component with loading state and success toast notification

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,36 +1,38 @@
-// "use client"
+"use client"
 
-// import { Loader2 } from 'lucide-react'
-// import { useState } from 'react'
-// import { Button } from '@/components/ui/button'
-// // import { resolve } from 'path'
-// import { Toaster } from 'sonner'
+import { Loader2 } from 'lucide-react'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+// import { resolve } from 'path'
+import { toast } from 'sonner'
 
-// const LogOutButton = () => {
-//     // const{toast} = useToast()
-//     const [loading, setLoading] = useState(false)
-//     const handleLogOut = async() => {
-//         setLoading(true)
-//         await new Promise( (resolve) => setTimeout(resolve, 2000));
-//         const errorMessage = null;
-//         if(!errorMessage){
-//             toast ({title:"Logged Out", description:"You have been logged out successfully", status: "success"})
-//         }
-//         setLoading(false)
-//         console.log("Logging out...")
-//         // setLoading(true)
-//         // setTimeout(() => {
-//         //     setLoading(false)
-//         // }, 2000)
-//     }
-//   return (
-//    <Button className='w-24'
-//    variant="outline"
-//    onClick={handleLogOut}
-//    >
-//     {loading ? <Loader2 className="animate-spin" /> : "Log Out"}
-//    </Button> 
-//   )
-// }
+const LogOutButton = () => {
+    // const{toast} = useToast()
+    const [loading, setLoading] = useState(false)
+    const handleLogOut = async() => {
+        setLoading(true)
+        await new Promise( (resolve) => setTimeout(resolve, 2000));
+        const errorMessage = null;
+        if(!errorMessage){
+            toast.success("Logged Out", {
+        description: "You have been logged out successfully",
+        })
+        }
+        setLoading(false)
+        console.log("Logging out...")
+        // setLoading(true)
+        // setTimeout(() => {
+        //     setLoading(false)
+        // }, 2000)
+    }
+    return (
+    <Button className='w-24'
+    variant="outline"
+    onClick={handleLogOut}
+    >
+    {loading ? <Loader2 className="animate-spin" /> : "Log Out"}
+    </Button> 
+    )
+}
 
-// export default LogOutButton
+export default LogOutButton
